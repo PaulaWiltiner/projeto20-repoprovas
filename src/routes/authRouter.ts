25449c33456a6bd131d0a8151349526f6bb401bd
PaulaWiltiner <paulawiltiner.pw@gmail.com>
2022-09-16 21:12:07 -0300

    correction signIn

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { signIn,signOut,signUp } from "../controllers/authController";
 import { validateSchema } from "../middlewares/schemaValidate";
+import { signInSchema } from "../schemas/signInSchema";
 import { signupSchema } from "../schemas/signUpSchema";
 const authRouter = Router();
 
 authRouter.post("/signUp",validateSchema(signupSchema), signUp);
-authRouter.post("/signIn",validateSchema(signupSchema),signIn )
+authRouter.post("/signIn",validateSchema(signInSchema),signIn )
 authRouter.put("/signOut",signOut)
 
 
--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -2,6 +2,6 @@ import Joi from 'joi';
 
 export const signupSchema = Joi.object({
   email: Joi.string().email().required(),
-  password: Joi.string().min(10).required(),
+  password: Joi.string().min(3).required(),
   samePassword: Joi.string().equal(Joi.ref('password')).required()
 });
