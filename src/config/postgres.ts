2255bffbc465947ea955f5ccf5d521e89cb40190
PaulaWiltiner <paulawiltiner.pw@gmail.com>
2022-09-17 21:09:01 -0300

    add pg

--- src/config/postgres.ts
+++ src/config/postgres.ts
@@ -5,5 +5,8 @@ dotenv.config();
 const { Pool } = pg;
 
 export const connection = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
