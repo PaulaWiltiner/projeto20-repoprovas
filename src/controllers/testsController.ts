bc5b3f59539a8da0d2a80b49f3b69456a4ba33be
PaulaWiltiner <paulawiltiner.pw@gmail.com>
2022-09-17 21:24:47 -0300

    add pg

--- src/controllers/testsController.ts
+++ src/controllers/testsController.ts
@@ -15,16 +15,16 @@ export async function createTest(req:Request, res:Response) {
 export async function getTestsDiscipline(req:Request, res:Response) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
-  await testsService.getTestsDiscipline(token)
+  const result = await testsService.getTestsDiscipline(token);
 
-  res.sendStatus(201);
+  res.send(result).status(200);
 }
 
 export async function getTestsTeacher(req:Request, res:Response) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
-  await testsService.getTestsTeacher(token)
+  const result= await testsService.getTestsTeacher(token)
 
-  res.sendStatus(201);
+  res.send(result).status(200);
 }
 
